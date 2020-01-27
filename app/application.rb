89e131cb56b54638563158c498b66ddd56925763
Julianna Boyd <julianna.boyd91@gmail.com>
2020-01-27 20:35:27 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,8 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
-
+  @@cart = []
+  
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
@@ -13,12 +14,19 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    
+    elsif req.path.match(/cart/) && @@cart.size == 0 
+      resp.write "Your cart is empty"
+      
+      
     else
       resp.write "Path Not Found"
     end
 
     resp.finish
   end
+
+  
 
   def handle_search(search_term)
     if @@items.include?(search_term)
